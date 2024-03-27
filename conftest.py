96ed43fad3eefb6e483357b4442719f1ab29c257
Uladzimir Bely <vladzimir.bely@gmail.com>
2024-03-27 16:28:46 +0300

    add create_zip function in conftest file

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import os
+import pytest
+from zipfile import ZipFile, ZIP_DEFLATED
+import pathlib
+
+@pytest.fixture(scope='function', autouse=True)
+def create_zip():
+    resources_path = pathlib.Path(os.path.join(os.path.dirname(__file__), os.path.abspath('resources')))
+    tmp_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tmp')
+    archive_path = os.path.join(tmp_path, 'archive.zip')
+
+    if not os.path.exists(tmp_path):
+        os.mkdir(tmp_path)
+
+    with ZipFile(archive_path, 'w', ZIP_DEFLATED) as zf:
+        for file in resources_path.iterdir():
+            zf.write(file, arcname=file.name)
