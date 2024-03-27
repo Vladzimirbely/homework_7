afb67d3cde02cff741f20606de54a8c89355dfa8
Uladzimir Bely <vladzimir.bely@gmail.com>
2024-03-27 16:44:43 +0300

    change path for archive

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ import pathlib
 @pytest.fixture(scope='function', autouse=True)
 def create_zip():
     resources_path = pathlib.Path(os.path.join(os.path.dirname(__file__), os.path.abspath('resources')))
-    tmp_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tmp')
+    tmp_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources/tmp')
     archive_path = os.path.join(tmp_path, 'archive.zip')
 
     if not os.path.exists(tmp_path):
